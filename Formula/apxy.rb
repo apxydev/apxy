6c734f03b6c810eeb71747873347d2fbf77c566d
Quang Truong Tuan <tuanquanghpvn@gmail.com>
2026-03-21 12:25:58 +0700

    chore: update Homebrew formula to v1.0.0

--- Formula/apxy.rb
+++ Formula/apxy.rb
@@ -1,22 +1,22 @@
 class Apxy < Formula
   desc "Desktop network proxy for HTTPS debugging — inspect, mock, debug"
   homepage "https://github.com/apxydev/apxy"
-  version "1.1.1"
+  version "1.0.0"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/apxydev/apxy/releases/download/v#{version}/apxy-#{version}-darwin-arm64.tar.gz"
-      sha256 "983a9520e17f2de9f8ab82938a6dc090e0f3f6c2e6d650c0a3be0c036f50b1ba"
+      url "https://github.com/apxydev/apxy/releases/download/v1.0.0/apxy-1.0.0-darwin-arm64.tar.gz"
+      sha256 "42da2e828f31b5bc7f682853d1ae4bf3d4d9587a32b804fc4761fcb1d46851f8"
     else
-      url "https://github.com/apxydev/apxy/releases/download/v#{version}/apxy-#{version}-darwin-amd64.tar.gz"
-      sha256 "7903ccf4fc0df5bc04029f816c015a5487176b31f6a31feadf9e224c5e9633e2"
+      url "https://github.com/apxydev/apxy/releases/download/v1.0.0/apxy-1.0.0-darwin-amd64.tar.gz"
+      sha256 "4fd3ff31238ce57a6516a710d1bf33517112ea6783857c9d5895d12dad4f49d4"
     end
   end
 
   on_linux do
-    url "https://github.com/apxydev/apxy/releases/download/v#{version}/apxy-#{version}-linux-amd64.tar.gz"
-    sha256 "bbaf9e813fa7068ecca44f43433f0785e9224aa82e3563a9362a15a9824d28b2"
+    url "https://github.com/apxydev/apxy/releases/download/v1.0.0/apxy-1.0.0-linux-amd64.tar.gz"
+    sha256 "223e2e514a91f3f54741d530785dc2e231fee861fa0a61169f43af4e5ae002fc"
   end
 
   def install
