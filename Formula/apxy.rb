8afefd053657a42af151ef193325747dfbce9ec7
Quang Truong Tuan <tuanquanghpvn@gmail.com>
2026-03-22 12:23:12 +0700

    chore: update Homebrew formula to v1.0.2

--- Formula/apxy.rb
+++ Formula/apxy.rb
@@ -1,13 +1,13 @@
 class Apxy < Formula
   desc "Desktop network proxy for HTTPS debugging — inspect, mock, debug"
   homepage "https://github.com/apxydev/apxy"
-  version "1.0.1"
+  version "1.0.2"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/apxydev/apxy/releases/download/v1.0.0/apxy-1.0.0-darwin-arm64.tar.gz"
-      sha256 "42da2e828f31b5bc7f682853d1ae4bf3d4d9587a32b804fc4761fcb1d46851f8"
+      url "https://github.com/apxydev/apxy/releases/download/v1.0.2/apxy-1.0.2-linux-amd64.tar.gz"
+    sha256 "bd06aad78b28d4eb51d6343d3623306be8ed17718b1b1108cdf4d3d3ce84c664"
     else
       url "https://github.com/apxydev/apxy/releases/download/v1.0.0/apxy-1.0.0-darwin-amd64.tar.gz"
       sha256 "4fd3ff31238ce57a6516a710d1bf33517112ea6783857c9d5895d12dad4f49d4"
