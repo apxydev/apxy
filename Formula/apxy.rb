7ff7de6246238d9e927d584f093b7d1229956f26
Quang Truong Tuan <tuanquanghpvn@gmail.com>
2026-04-20 20:23:34 +0700

    chore: update Homebrew formula to v1.1.7

--- Formula/apxy.rb
+++ Formula/apxy.rb
@@ -1,26 +1,26 @@
 class Apxy < Formula
   desc "Desktop network proxy for HTTPS debugging — inspect, mock, debug"
   homepage "https://github.com/apxydev/apxy"
-  version "1.1.6"
+  version "1.1.7"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/apxydev/apxy/releases/download/v1.1.6/apxy-1.1.6-darwin-arm64.tar.gz"
-      sha256 "00454c4fdc4a6686fa9534a19239792b8da592a42db62c370c708053d636b482"
+      url "https://github.com/apxydev/apxy/releases/download/v1.1.7/apxy-1.1.7-darwin-arm64.tar.gz"
+      sha256 "666087d1a24d8efda8a604a8f550cfc8ee69525109a7d8e1cd48d443da45a15d"
     else
-      url "https://github.com/apxydev/apxy/releases/download/v1.1.6/apxy-1.1.6-darwin-amd64.tar.gz"
-      sha256 "5490d46e0048ef8724bbc88ec405512e17113f804e59dd6ed9b5333859fc6f8b"
+      url "https://github.com/apxydev/apxy/releases/download/v1.1.7/apxy-1.1.7-darwin-amd64.tar.gz"
+      sha256 "f70ee44e347cc50504cc4991c453e5a157ed8a39a42175060abda48df8b6d9a4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/apxydev/apxy/releases/download/v1.1.6/apxy-1.1.6-linux-arm64.tar.gz"
-      sha256 "e4dba867555e4ca742a60ed403f2f5e54823a4ef738c87be022f78a807c68640"
+      url "https://github.com/apxydev/apxy/releases/download/v1.1.7/apxy-1.1.7-linux-arm64.tar.gz"
+      sha256 "0dbd3af7475678c163b35fc4e31669f16d51b14e9397d6d06a1b536819a64f88"
     else
-      url "https://github.com/apxydev/apxy/releases/download/v1.1.6/apxy-1.1.6-linux-amd64.tar.gz"
-      sha256 "293f9355fc86e5b1cddf3aa04fd040d5679b29210522661089de5c9bbe74487d"
+      url "https://github.com/apxydev/apxy/releases/download/v1.1.7/apxy-1.1.7-linux-amd64.tar.gz"
+      sha256 "dc9c2651f968e5dcc461e1e07fd01fc5b8cbc1e5565f66dfb56f32d46a3a192e"
     end
   end
 
